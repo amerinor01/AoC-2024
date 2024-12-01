738c8317770c824c0b33608d06f6f866d95de0ac
Alberto Merino <amerinor01@gmail.com>
2024-12-01 17:56:33 +0100

    day 1

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake for building the simulator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        code = pkgs.callPackage ./. { inherit nixpkgs system rust-overlay; };
+      in
+      rec {
+        packages = {
+          simulator = code.simulator;
+          default = packages.simulator;
+        };
+      }
+    );
+}
---
+++ src/day1.rs
@@ -0,0 +1,73 @@
+use std::fs;
+use std::collections::HashMap;
+
+pub fn first() {
+        // --snip--
+    let file_path = String::from("inputs/day1.txt");
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    
+    let v = contents.split('\n').flat_map(|str| str.split("   ")).map(str::to_owned);
+    let mut merged = v.collect::<Vec<_>>();
+    merged.pop();
+    
+    let mut odd: Vec<i32> = merged
+        .iter()
+        .skip(1)
+        .step_by(2)
+        .map(|value| value.parse().unwrap())
+        .collect();
+    odd.sort();
+    let mut even: Vec<i32> = merged
+        .iter()
+        .step_by(2)
+        .map(|value| value.parse().unwrap())
+        .collect();
+    even.sort();
+    
+    let result: i32 = odd.iter().zip(even.iter())
+        .map(|(a, b)| i32::abs(a - b))
+        .sum();
+
+    println!("{:?}", result);
+}
+
+pub fn second() {
+        // --snip--
+    let file_path = String::from("inputs/day1.txt");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    
+    let mut merged: Vec<_>  = contents.split('\n').flat_map(|str| str.split("   ")).map(str::to_owned).collect();
+    //let mut merged = v.collect::<Vec<_>>();
+    merged.pop();
+
+    let mut odd: Vec<i32> = merged
+        .iter()
+        .skip(1)
+        .step_by(2)
+        .map(|value| value.parse().unwrap())
+        .collect();
+    odd.sort();
+    let mut even: Vec<i32> = merged
+        .iter()
+        .step_by(2)
+        .map(|value| value.parse().unwrap())
+        .collect();
+    even.sort();
+    
+    let appears: HashMap<_,_> =  odd.iter().fold(HashMap::new(), |mut acc, &item| {
+        *acc.entry(item).or_insert(0) += 1;
+        acc
+    });
+    
+    let result: i32 = even.iter()
+    .map(|value| {
+            appears.get(value).unwrap_or(&0) * value
+        })
+    .sum();
+
+    println!("{:?}", result);
+
+}
